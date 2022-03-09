75859cbb2ed9ce0c80e73ab6659547578346c25f
TomCat65 <talvarez.uh@gmail.com>
2022-03-09 01:14:49 -0600

    nextjs with query-react demo

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,17 @@
+import React from "react"
+import {Hydrate,QueryClient,QueryClientProvider} from 'react-query'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const queryClient=React.useRef(new QueryClient())
+  return (
+  <QueryClientProvider client={queryClient.current}>
+    <Hydrate state={pageProps.dehydratedState}>
+      <Component {...pageProps} />
+      </Hydrate>
+     
+    </QueryClientProvider>
+ )
 }
 
 export default MyApp
